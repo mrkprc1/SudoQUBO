0ebcd91a5d2d66835780a6fa0579850196951c10
kossick <tjbullock89@gmail.com>
2020-10-06 09:50:35 +0300

    Added timing and alternative plotting function

--- test.py
+++ test.py
@@ -3,6 +3,27 @@ import pandas as pd
 from qubovert import boolean_var
 from qubovert.sim import anneal_qubo
 import matplotlib.pyplot as plt 
+from matplotlib.colors import ListedColormap
+import seaborn as sns
+import time
+
+time0 = time.time()
+
+def alt_plot_mat(matrix):
+    fig, ax = plt.subplots(figsize=(5,5))
+
+    ax = sns.heatmap(data=matrix, 
+        cbar=False, 
+        annot=True, 
+        cmap=ListedColormap(['white']), 
+        yticklabels=False, 
+        xticklabels=False
+    )
+    ax.hlines([0, 3, 6, 9], linewidth=2, *ax.get_xlim())
+    ax.hlines([1, 2, 4, 5, 7, 8], linewidth=0.5, *ax.get_xlim())
+    ax.vlines([0, 3, 6, 9], linewidth=2, *ax.get_ylim())
+    ax.vlines([1, 2, 4, 5, 7, 8], linewidth=0.5, *ax.get_xlim())
+
 
 def plot_mat(matrix, values=True):
     n = matrix.shape[0]
@@ -17,6 +38,8 @@ def plot_mat(matrix, values=True):
                 ax.text(i, j, str(matrix[n-j-1,i]), va='center', ha='center')
         ax.set_xlim(-0.5, n-0.5)
         ax.set_ylim(-0.5, n-0.5)
+        # ax.hlines([3, 6, 9], *ax.get_xlim())
+        # ax.vlines([3, 6, 9], *ax.get_ylim())
         #ax.set_xticks(np.arange(max_val))
         #ax.set_yticks(np.arange(max_val))
     else:
@@ -41,7 +64,7 @@ idx = np.array([k for (k, v) in x_init.items() if v])
 init_vec[idx] = 1
 
 sudoku_init = (np.kron(np.identity(N**2, dtype=np.int64), z) @ init_vec).reshape((N, N))
-plot_mat(sudoku_init)
+alt_plot_mat(sudoku_init)
 
 # Define binary variables for QUBO.
 x = {i : boolean_var(f'x{i}') for i in range(num_bits) if i not in x_init.keys()}
@@ -62,14 +85,18 @@ b = np.ones((4*N*N, 1))
 
 Q = penalty * (A.T @ A - 2 * np.diag((b.T @ A)[0]))
 
+time1 = time.time()
+
 # Define model.
 model = penalty * (b.T @ b)[0,0]
 for i in range(num_bits):
     for j in range(num_bits):
         model += Q[i,j]*x[i]*x[j]
 
+time2 = time.time()
+
 # Run simulated annealing.
-res = anneal_qubo(model, num_anneals=3)
+res = anneal_qubo(model, num_anneals=50)
 
 # Check solution.
 if res.best.value == 0:
@@ -85,4 +112,10 @@ sol_vec[idx] = 1
 
 sudoku_sol = (np.kron(np.identity(N**2, dtype=np.int64), z) @ sol_vec).reshape((N, N))
 
-plot_mat(sudoku_sol)
+alt_plot_mat(sudoku_sol)
+
+time3 = time.time()
+
+print(f"initialisation time: {round(time1 - time0, 3)}s")
+print(f"model time: {round(time2 - time1, 3)}s")
+print(f"anneal time: {round(time3 - time2, 3)}s")
